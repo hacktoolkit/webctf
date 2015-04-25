90b023999f0c0b7df8a799d6542663fdf66a8b1e
Jonathan Tsai <jontsai@chegg.com>
2015-04-24 17:48:30 -0700

    progress commit

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-if (PARSE_APPLICATION_ID && PARSE_JS_KEY) {
-    Parse.initialize(PARSE_APPLICATION_ID, PARSE_JS_KEY);
-}
-
-/*
-  var TestObject = Parse.Object.extend("TestObject");
-  var testObject = new TestObject();
-  testObject.save({foo: "bar"}, {
-  success: function(object) {
-  $(".success").show();
-  },
-  error: function(model, error) {
-  $(".error").show();
-  }
-  });
-*/
-
 YUI().use(
     'node',
     'event',
@@ -87,14 +70,24 @@ function (Y) {
         syncLoginButtons();
     }
 
+    function handleTryItClicked(e) {
+        executeConsole();        
+    }
+
+    function executeConsole() {
+        var consoleContent = $('.webconsole').val();
+        $('.output').html(consoleContent);
+    }
+
     // App Initializers
     function initEventHandlers() {
         Y.delegate('click', handleLoginClicked, '#header', '#' + CSS_ID_LOGIN_BUTTON);
         Y.delegate('click', handleLogoutClicked, '#header', '#' + CSS_ID_LOGOUT_BUTTON);
+        main.delegate('click', handleTryItClicked, '.try-it');
     }
 
     function init() {
-        syncLoginButtons();
+        //syncLoginButtons();
     }
     initEventHandlers();
     init();
